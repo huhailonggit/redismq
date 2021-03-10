58bfe2f09dd65133602d13ae902fd88eb9ddbdb6
胡海龙 <hailong.hu@bj-tct.com>
2021-03-10 16:53:34 +0800

    initialization project

---
+++ src/main/java/vip/huhailong/redismq/RedismqApplication.java
@@ -0,0 +1,13 @@
+package vip.huhailong.redismq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedismqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedismqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vip/huhailong/redismq/RedismqApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.huhailong.redismq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedismqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
